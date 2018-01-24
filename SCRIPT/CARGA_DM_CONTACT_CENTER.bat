7f69e7c49e3ed819f02c4f22b2d550fadbdb9874
GONZALO-DIAZ-D <35373117+GONZALO-DIAZ-D@users.noreply.github.com>
2018-01-24 09:23:33 -0300

    Update CARGA_DM_CONTACT_CENTER.bat

--- SCRIPT/CARGA_DM_CONTACT_CENTER.bat
+++ SCRIPT/CARGA_DM_CONTACT_CENTER.bat
@@ -1,6 +1,6 @@
  C:
  cd "C:\Pentaho\data-integration"
- kitchen.bat /rep:"REPOSITORIO_DI_PROD" /job:"CARGA_DM_CONTACT_CENTER" /dir:"/COMERCIAL/DM_CONTACT_CENTER/" /user:GONZALO-DIAZ /pass:DI2018prod /level:Basic > "E:\ETLs\LOG\CARGA_DM_CONTACT_CENTER_%Date:~6,4%%Date:~3,2%%Date:~0,2%_%time:~0,2%%time:~3,2%.log"
+ kitchen.bat /rep:"REPOSITORIO_DI_PROD" /job:"CARGA_DM_CONTACT_CENTER" /dir:"/COMERCIAL/DM_CONTACT_CENTER/" /user:GONZALO-DIAZ /pass:DI2018prod /level:Basic
 
 
 
